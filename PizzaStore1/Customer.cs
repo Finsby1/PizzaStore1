a2018dd36923f04cb4583d2d524802174b422af1
Benjamin Lucas Jensen <bej002@edu.zealand.dk>
2024-03-03 18:26:07 +0100

    WIP

---
+++ PizzaStore1/Customer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaStore1
+{
+    internal class Customer
+    {
+        string _name;
+        string _phonenr;
+        string _email;
+        string _address;
+
+        public Customer()
+        {
+            _name = "";
+            _phonenr = "";
+            _email = "";
+            _address = ""; 
+        }
+
+        public Customer(string name, string phonenr, string email, string address)
+        {
+            _name = name;
+            _phonenr = phonenr;
+            _email = email;
+            _address = address;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Phonenr
+        {
+            get { return _phonenr; }
+            set { _phonenr = value; }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+    
+
+        public override string ToString()
+        {
+            return $"Name: {Name} - Phone nr. {Phonenr} - E-mail {Email} - Address {Address}";
+        }
+    }
+}
---
+++ PizzaStore1/Order.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaStore1
+{
+    public class Order
+    {
+        double _tax;
+        double _deliveryCost;
+        Pizza p;
+        Customer c;
+
+        public Order(Pizza pizza, Customer customer)
+        {
+            p = pizza;
+            Tax = 2;
+            _deliveryCost = 30;
+        }
+
+        double Tax
+        {
+            get { return _tax; }
+            set { _tax = value; }
+        }
+
+        double DeliveryCost
+        {
+            get { return _deliveryCost; }
+            set { _deliveryCost = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Pizza: {p.Name} - Tax: {Tax} - Delivery Cost: {DeliveryCost} - Total: {CalculateTotalPrice()}";
+        }
+
+        public double CalculateTotalPrice()
+        {
+            return p.Price * (1 + Tax / 100) + DeliveryCost;
+        }
+    }
+
+}
